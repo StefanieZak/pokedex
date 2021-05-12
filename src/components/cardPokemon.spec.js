a9e203f3e4dd1a8eed0b0a9eea8c8759e5c69f26
Eduardo Fernandes <eduardo_2407@me.com>
2021-05-12 14:54:26 -0300

    cardPokemon

--- src/components/cardPokemon.spec.js
+++ src/components/cardPokemon.spec.js
@@ -1,35 +1,37 @@
-// /* eslint-disable no-undef */
-// import { shallowMount, createLocalVue } from '@vue/test-utils'
-// import CardPokemon from '@/components/CardPokemon.vue'
-// import VueRouter from 'vue-router'
+/* eslint-disable no-undef */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Header from '@/components/Header.vue'
+import VueRouter from 'vue-router'
 
-// const localVue = createLocalVue()
-// localVue.use(VueRouter)
-// const router = new VueRouter({
-//   routes: [
-//     {
-//       name: 'Home',
-//       path: '/'
-//     }
-//   ]
-// });
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    {
+      name: 'Home',
+      path: '/'
+    }
+  ]
+});
 
 
-// describe('CardPokemon.vue', () => {
-//   it('should show correctly card.', async () => {
-//     localVue,
-//     router
-//     const wrapper = shallowMount(CardPokemon, {
-//       stubs: ['router-link'],
-//       props: { name: 'bulbasaur', url:'https://pokeapi.co/api/v2/pokemon/1/'}
-//     })
-//     await wrapper.setData({ 
-//       itemPokemon: "t",
-//       typeCard: "e",
-//       bgType:"s",
-//       cardByRoute: "t",
-//       labelByRoute: "e",
-//      })
-//     expect(wrapper.classes()).toContain('card')
-//   })
-// })
+describe('Header.vue', () => {
+  it('should show correctly the title.', async () => {
+    localVue,
+    router
+    const title = 'Pokedex'
+    const wrapper = shallowMount(Header, {
+      stubs: ['router-link']
+    })
+    expect(wrapper.text()).toMatch(title)
+  })
+
+  it('input field must exist', () => {
+    localVue,
+    router
+    const wrapper = shallowMount(Header, {
+      stubs: ['router-link']
+    })
+    expect(wrapper.find('#search').exists()).toBeTruthy()
+  }) 
+})
