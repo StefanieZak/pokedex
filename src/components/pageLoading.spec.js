7e4a04197080afcd871a1ab77f4261d702d22eb6
Eduardo Fernandes <eduardo_2407@me.com>
2021-05-12 11:38:35 -0300

    testes

---
+++ src/components/pageLoading.spec.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Header from '@/components/PageLoading.vue'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    {
+      name: 'Home',
+      path: '/'
+    }
+  ]
+});
+
+
+describe('Header.vue', () => {
+  it('should show correctly the title.', () => {
+    localVue,
+    router
+    const msg = 'Loading...'
+    const wrapper = shallowMount(Header, {
+      stubs: ['router-link']
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+
+  it('input field must exist', () => {
+    localVue,
+    router
+    const wrapper = shallowMount(Header, {
+      stubs: ['router-link']
+    })
+    expect(wrapper.find('.loading').exists()).toBeTruthy()
+  }) 
+})
