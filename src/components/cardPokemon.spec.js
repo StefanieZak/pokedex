15ab20c28b23fdbe9eda38bc2f21eda2b7aa5649
StefanieZak <stefaniezak@hotmail.com>
2021-05-12 16:25:46 -0300

    creating test for NotFound

--- src/components/cardPokemon.spec.js
+++ src/components/cardPokemon.spec.js
@@ -1,37 +1,34 @@
 /* eslint-disable no-undef */
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Header from '@/components/Header.vue'
-import VueRouter from 'vue-router'
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Header from "@/components/Header.vue";
+import VueRouter from "vue-router";
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
+const localVue = createLocalVue();
+localVue.use(VueRouter);
 const router = new VueRouter({
   routes: [
     {
-      name: 'Home',
-      path: '/'
-    }
-  ]
+      name: "Home",
+      path: "/",
+    },
+  ],
 });
 
-
-describe('Header.vue', () => {
-  it('should show correctly the title.', async () => {
-    localVue,
-    router
-    const title = 'Pokedex'
+describe("Header.vue", () => {
+  it("should show correctly the title.", async () => {
+    localVue, router;
+    const title = "Pokedex";
     const wrapper = shallowMount(Header, {
-      stubs: ['router-link']
-    })
-    expect(wrapper.text()).toMatch(title)
-  })
+      stubs: ["router-link"],
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
 
-  it('input field must exist', () => {
-    localVue,
-    router
+  it("input field must exist", () => {
+    localVue, router;
     const wrapper = shallowMount(Header, {
-      stubs: ['router-link']
-    })
-    expect(wrapper.find('#search').exists()).toBeTruthy()
-  }) 
-})
+      stubs: ["router-link"],
+    });
+    expect(wrapper.find("#search").exists()).toBeTruthy();
+  });
+});
